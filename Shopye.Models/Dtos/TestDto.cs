2c98f447f64f19386787bde053946084a966c7ca
Mahmoud Reda <mahmoud.reda@softec.ai>
2022-06-08 10:12:49 +0200

    wqeqwetutorial components

---
+++ Shopye.Models/Dtos/TestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopye.Models.Dtos
+{
+    public class TestDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Description custom message")]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
